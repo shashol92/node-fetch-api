4165853931b29fa94e6b26697bc9e0036c5b4c1f
Shashikumar OL <shashikumarol@Shashikumars-MacBook-Air.local>
2022-07-14 17:35:06 +0530

    scripts added

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const fetch = require("node-fetch");
 const app = express();
 
 app.get('/', (req, res)=> {
-    // res.send("Hello World");
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(json => {
-        res.json(json);
-})
+    res.send("Hello World");
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    // .then(res => res.json())
+    // .then(json => {
+    //     res.json(json);
+    // })
 });
 
 app.listen(3000);
