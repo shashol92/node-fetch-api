d6a6eb534ea887853847928c78ea7416ef8924db
Shashikumar OL <shashikumarol@Shashikumars-MacBook-Air.local>
2022-07-14 16:22:24 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+
+app.get('/', (req, res)=> {
+    // res.send("Hello World");
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => {
+        res.json(json);
+})
+});
+
+app.listen(3000);
